e0510e31ab03ab45d4c66c6ff891bb806771aaa9
Mason Ritchason <masonritchason@gmail.com>
2025-07-11 15:40:09 -0400

    feat(PartialDataSet): implement recursive Parse() method
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCom/Types/PartialDataSet.cs
+++ LotCom/Types/PartialDataSet.cs
@@ -72,6 +72,74 @@ public partial class PartialDataSet(Quantity Quantity, Shift Shift, Operator Ope
     }
 
     /// <summary>
+    /// Recursively parses 1 to 3 PartialDataSets from Quantity, Shift, and Operator.
+    /// </summary>
+    /// <remarks>
+    /// Expected argument formats:
+    /// ["123", "1", "ABC"] (Base, single parse) OR 
+    /// ["123:456:789", "1:2:3", "ABC:DEF:GHI"] (Recursive, 2-3 parses).
+    /// </remarks>
+    /// <param name="Quantity">A string of digits or sets of digits separated by colons.</param>
+    /// <param name="Shift">A string of a single digit or a set of single digits separated by colons.</param>
+    /// <param name="Operator">A string of letters or sets of letters separated by colons.</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static List<PartialDataSet> Parse(string Quantity, string Shift, string Operator)
+    {
+        List<PartialDataSet> ParsedSets = [];
+        // RECURSIVE CASE: colon in quantity field indicates the existence of multiple PartialDataSets
+        if (Quantity.Contains(':'))
+        {
+            // split passed fields into lists
+            string[] SplitQuantity = Quantity.Split(":");
+            string[] SplitShift = Shift.Split(":");
+            string[] SplitOperator = Operator.Split(":");
+            // for loop over 2 or 3 possible PartialDataSet
+            for (int i = 0; i < SplitQuantity.Length; i++)
+            {
+                // parse and add parsed PartialDataSets to the return list
+                try
+                {
+                    ParsedSets.Add(Parse(SplitQuantity[0], SplitShift[0], SplitOperator[0])[0]);
+                }
+                catch (ArgumentException)
+                {
+                    throw;
+                }
+            }
+        }
+        // BASE CASE: there is a single PartialDataSet present to parse from the CSV fields
+        else
+        {
+            try
+            {
+                // construct a single PartialDataSet from the passed field values
+                ParsedSets.Add
+                (
+                    new PartialDataSet
+                    (
+                        new Quantity(int.Parse(Quantity)),
+                        ShiftExtensions.FromString(Shift),
+                        new Operator(Operator)
+                    )
+                );
+            }
+            // the int.Parse() call faulted
+            catch (FormatException)
+            {
+                throw new ArgumentException($"Could not parse int Quantity from '{Quantity}'.");
+            }
+            // one of the three Quantity, Shift, Operator parses faulted
+            catch (ArgumentException)
+            {
+                throw new ArgumentException($"Could not parse a PartialDataSet from '{Quantity}, {Shift}, {Operator}'.");
+            }
+        }
+        // return up to 3 PartialDataSet objects
+        return ParsedSets;
+    }
+
+    /// <summary>
     /// Validates that the DataSet's Quantity is greater than 0.
     /// </summary>
     /// <exception cref="ArgumentException"></exception>
