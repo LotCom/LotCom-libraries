965b0ecc7a15c77c9a12f2a4fbae4ea223f8e8c6
Mason Ritchason <masonritchason@gmail.com>
2025-08-04 17:07:57 -0400

    feat(HttpClientFactory): implement HttpClientFactory class
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

---
+++ LotCom/DataAccess/HttpClientFactory.cs
@@ -0,0 +1,38 @@
+using System.Net.Http.Headers;
+using LotCom.DataAccess.Extensions;
+
+namespace LotCom.DataAccess;
+
+/// <summary>
+/// Provides controlled creation of HttpClient objects that meet the standards of the LotCom API.
+/// </summary>
+public static class HttpClientFactory
+{
+    /// <summary>
+    /// Creates an HTTP request/response client that is configured for a LotCom system User Agent.
+    /// </summary>
+    public static HttpClient Create(UserAgent Agent)
+    {
+        HttpClient Client = new HttpClient();
+        // cleans the HttpClient's accepted response header configuration
+        Client.DefaultRequestHeaders.Accept.Clear();
+        // adds the default API response header to the accepted response configuration
+        Client.DefaultRequestHeaders.Accept.Add
+        (
+            new MediaTypeWithQualityHeaderValue
+            (
+                "application/json"
+            )
+        );
+        // adds the default UA header for custom apps (ex. LotCom Printer 1.0.0)
+        Client.DefaultRequestHeaders.Add
+        (
+            "User-Agent",
+            $"LotCom"
+            + $"{AppNameExtensions.ToString(Agent.AppName)}"
+            + $"/{Agent.AppVersion}"
+            + $" ({AppPlatformExtensions.ToString(Agent.Platform)}"
+            + $"; {AppEnvironmentExtensions.ToString(Agent.Environment)})");
+        return Client;
+    }
+}
