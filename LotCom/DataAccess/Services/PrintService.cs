53f3cd37c3d443d8709a2e171c85943b127afcc3
Mason Ritchason <masonritchason@gmail.com>
2025-08-05 11:28:54 -0400

    feat(PrintService): implement GetOnDateByProcess method
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCom/DataAccess/Services/PrintService.cs
+++ LotCom/DataAccess/Services/PrintService.cs
@@ -17,7 +17,7 @@ public static class PrintService
     /// <param name="Agent"></param>
     /// <returns></returns>
     /// <exception cref="JsonException"></exception>
-    public static async Task<Print>? Get(int id, UserAgent Agent)
+    public static async Task<Print?> Get(int id, UserAgent Agent)
     {
         HttpClient Client = HttpClientFactory.Create(Agent);
         HttpResponseMessage? Response = await Client.GetAsync($"http://localhost:60000/Print/{id}");
@@ -38,5 +38,47 @@ public static class PrintService
             throw new JsonException("Could not deserialize a Print from the response.");
         }
         return await PrintMapper.DtoToModel(Dto, Agent);
+    }
+
+    /// <summary>
+    /// Retrieves all Prints produced by ProcessId on Date from the Database.
+    /// </summary>
+    /// <param name="Date"></param>
+    /// <param name="ProcessId"></param>
+    /// <param name="Agent"></param>
+    /// <returns></returns>
+    /// <exception cref="JsonException"></exception>
+    public static async Task<IEnumerable<Print>?> GetOnDateByProcess(DateTime Date, int ProcessId, UserAgent Agent)
+    {
+        HttpClient Client = HttpClientFactory.Create(Agent);
+        HttpResponseMessage? Response = await Client.GetAsync
+        (
+            $"http://localhost:60000/Print/onDateBy?" + 
+            $"day={Date.Day}" +
+            $"&month={Date.Month}" +
+            $"&year={Date.Year}" +
+            $"&processId={ProcessId}"
+        );
+        // ensure that the response was OK and retrieve its contents as JSON
+        try
+        {
+            Response.EnsureSuccessStatusCode();
+        }
+        catch (HttpRequestException)
+        {
+            throw;
+        }
+        string JSON = await Response.Content.ReadAsStringAsync();
+        // deserialize the JSON response and map the data from DTO to Model
+        IEnumerable<PrintDto>? Dtos = JsonConvert.DeserializeObject<IEnumerable<PrintDto>>(JSON);
+        if (Dtos is null)
+        {
+            throw new JsonException("Could not deserialize Prints from the response.");
+        }
+        IEnumerable<Print> Prints = await Task.WhenAll
+        (
+            Dtos.Select(async x => await PrintMapper.DtoToModel(x, Agent))
+        );
+        return Prints;
     }
 }
