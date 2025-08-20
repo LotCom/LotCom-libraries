6e43fa73bbd06b979c598f8b986257297efedac8
Mason Ritchason <masonritchason@gmail.com>
2025-08-20 14:30:42 -0400

    feat(ProcessService): implement GetAll method
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCom/DataAccess/Services/ProcessService.cs
+++ LotCom/DataAccess/Services/ProcessService.cs
@@ -7,7 +7,39 @@ namespace LotCom.DataAccess.Services;
 
 public static class ProcessService
 {
-    
+    /// <summary>
+    /// Retrieves all Processes from the database.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="HttpRequestException"></exception>
+    /// <exception cref="JsonException"></exception>
+    public static async Task<IEnumerable<Process>?> GetAll(UserAgent Agent)
+    {
+        HttpClient Client = HttpClientFactory.Create(Agent);
+        HttpResponseMessage? Response = await Client.GetAsync("http://localhost:60000/Process");
+        // ensure that the response was OK and retrieve its contents as JSON
+        try
+        {
+            Response.EnsureSuccessStatusCode();
+        }
+        catch (HttpRequestException)
+        {
+            throw;
+        }
+        string JSON = await Response.Content.ReadAsStringAsync();
+        // deserialize the JSON response and map the data from Dao to Model
+        IEnumerable<ProcessDao>? Daos = JsonConvert.DeserializeObject<IEnumerable<ProcessDao>>(JSON);
+        if (Daos is null)
+        {
+            throw new JsonException("Could not deserialize Processes from the response.");
+        }
+        IEnumerable<Process> Processes = await Task.WhenAll
+        (
+            Daos.Select(async x => await ProcessMapper.DaoToModel(x, Agent))
+        );
+        return Processes;
+    }
+
     /// <summary>
     /// Retrieves a single Process from the database using its Id.
     /// </summary>
@@ -29,12 +61,12 @@ public static class ProcessService
             throw;
         }
         string JSON = await Response.Content.ReadAsStringAsync();
-        // deserialize the JSON response and map the data from DTO to Model
-        ProcessDto? Dto = JsonConvert.DeserializeObject<ProcessDto>(JSON);
-        if (Dto is null)
+        // deserialize the JSON response and map the data from Dao to Model
+        ProcessDao? Dao = JsonConvert.DeserializeObject<ProcessDao>(JSON);
+        if (Dao is null)
         {
             throw new JsonException("Could not deserialize a Process from the response.");
         }
-        return await ProcessMapper.DtoToModel(Dto, Agent);
+        return await ProcessMapper.DaoToModel(Dao, Agent);
     }
 }
